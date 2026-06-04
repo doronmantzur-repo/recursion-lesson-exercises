37b25065df92abdc2f7b4b5e205786f018c30095
doronm <doronm@chain-reaction.io>
2026-06-04 17:18:22 +0300

    Update dependencies and implement core functions
    - Updated jest-cli and jest to version 30.4.2 in package.json for improved testing capabilities.
    - Refactored findFactorial function to correctly calculate the factorial of a number using recursion.
    - Implemented reverseString function to reverse a given string using recursion and array manipulation.
    - Enhanced swap function to transfer elements from arr1 to arr2 recursively and log the results.

--- src/app.js
+++ src/app.js
@@ -4,25 +4,44 @@
 */
 
 //Exercise 1
-const findFactorial = function() {
-  //Your code here
-
+const findFactorial = function(num) {
+  if (num === 0) return 1;
+  factorial = num * findFactorial(num - 1);
+  return factorial;
 }
 
 //Exercise 2
-const reverseString = function() {
-  //Your code here
-
+const reverseString = function(str) {
+  const strArr = str.split("");
+  let middleStr = "";
+  const length = strArr.length;
+  if (length === 1) return str;
+  const lastChar = strArr[length -1];
+  strArr[length -1] = strArr[0];
+  strArr[0] = lastChar;
+  middleStr = reverseString((strArr.slice(1, length-1)).join(""));
+  newArr = strArr.splice(1, length-2, ...middleStr.split(""));
+  str = strArr.join("");
+  return str;
 }
+
+
 
 //Exercise 3
 const arr1 = [1, 2, 3]
 const arr2 = []
 
-const swap = function() {
-  //Your code here
-
+const swap = function(arr) {
+  arr2.push(arr[0]);
+  arr1.splice(0,1);
+  if (arr.length === 0){
+    console.log("arr1:", arr1);
+    console.log("arr2:", arr2);
+    return;
+  } 
+  swap(arr1); 
 }
 
+swap(arr1)
 /* DO NOT REMOVE THE EXPORTS BELOW */
 module.exports = { findFactorial, reverseString, swap }
